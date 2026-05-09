dbdd490cf564e9b6f68415ef5e201fe486e1231a
JohannAlpapara <johannpaulalpapara@gmail.com>
2026-05-09 14:37:10 +0800

    Added changes for development only

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["*","192.168.1.236"],
+  allowedDevOrigins: ["192.168.1.236", "172.16.6.65"],
 };
 
 export default nextConfig;
